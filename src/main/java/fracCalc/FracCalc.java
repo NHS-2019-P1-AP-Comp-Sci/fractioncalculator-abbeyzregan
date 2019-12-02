c33a8cf20a7a83b70d817f0287b823ada02f5bec
s-regana <s-regana@53S50030585.it.bsd405.org>
2019-12-01 21:00:24 -0800

    Checkpoint 3 : Passes Checkpoint 3 but not the final test yet.

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,7 +3,8 @@
  */
 
 package fracCalc;
-import java.util.*; 
+import java.util.*;
+import java.lang.Math.*; 
 public class FracCalc {
 
     public static void main(String[] args)
@@ -44,12 +45,12 @@ public class FracCalc {
         // TODO: Implement this function to produce the solution to the input
     	operand1 = firstNum(input);
     	operand2 = operator(input);
-    	charPlacement = input.indexOf(operand2 , 1);
-    	charPlacement +=  2 ; 
+    	charPlacement = operand1.length() + 3 ; 
     	operand3 = secondNum (input , charPlacement); // used to store value for 1st test
-    	String operand3Check2 = seperateNumber(operand3);// stores value for 2nd test
-    	
-        return operand3Check2 ; // 1st test : operand 3 ; 2nd test : operand3Check2 ; 
+    	int[] num1 = mixedFrac(operand1);
+    	int[] num3 = mixedFrac(operand3);// stores value for 2nd test
+    	String answer = calculateAnswer(num1, num3, operand2 );
+        return answer ; // 1st test : operand 3 ; 2nd test : operand3Check2 ; 
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
@@ -89,8 +90,8 @@ public class FracCalc {
     	}
     	return operand ; 
     }
-    // seperates out the variables and calculates them out into their selective parts. 
-    public static String seperateNumber (String input) { 
+    // separates out the variables and calculates them out into their selective parts. 
+    public static int[] mixedFrac (String input) { 
     	int whole ; // the int value of the whole number.
     	int numerator ; // int value of the numerator ; 
     	int denominator ; // int value of the denominator
@@ -155,11 +156,90 @@ public class FracCalc {
     			denominator = Integer.parseInt(denomSubstr);
     		}
     	}
-    	String checkpoint2 = "whole:" + whole + " numerator:" + numerator + " denominator:" + 
-    			denominator ;
-    	// finds the double value of all of the terms combined into one number. 
-    	double value = ( (numerator * 1.0 ) / ( denominator * 1.0 ) ) + whole ;
-    	return checkpoint2; 
+    	int[] answer = {whole , numerator , denominator }; // array of num values
+		return answer ;  
     }
-    
+   public static String calculateAnswer(int[] num1 , int[] num2 , char operand) {
+	   int numerator1 ; // stores numerator of the first fraction
+	   int numerator2 ; // stores numerator of the second fraction
+	   int denominator1 = num1[2]; // stores the denominator of the first fraction
+	   int denominator2 = num2[2]; // stores the denominator of the second fraction
+	   int numerator ; // holds the numerator of the function
+	   int denominator ; // holds the denominator of the function
+	   int whole ; // holds the whole value of the mixed fraction
+	   if (num1[0] < 0 ) { // converts whole number to frac for negative numbers
+		   numerator1 = (-1 ) * ( (Math.abs(num1[0]) * num1[2] ) + num1[1] ); 
+	   } else { // handles the conversion of positive fractions to numerators and negative numbers. 
+		   numerator1 = (num1[0] * num1[2]) + num1[1];
+	   }
+	   if (num2[0] < 0 ) { // converts whole number to frac for negative numbers
+		   numerator2 = (-1 ) * ( (Math.abs(num2[0]) * num2[2] ) + num2[1] ); 
+	   } else { // handles the conversion of positive fractions to numerators and negative numbers. 
+		   numerator2 = (num2[0] * num2[2]) + num2[1];
+	   }
+	   if (operand == '+') { // handles the addition of fractions
+		   numerator = (numerator1 * denominator2) + (numerator2 * denominator1 ); 
+		   denominator = denominator1 * denominator2 ; 
+	   } else if (operand == '-') { // handles the subtraction of fractions
+		   numerator = (numerator1 * denominator2) - (numerator2 * denominator1);
+		   denominator = denominator1 * denominator2 ; 
+	   } else if (operand == '*') { // handles the multiplication of fractions
+		   numerator = numerator1 * numerator2 ; 
+		   denominator = denominator1 * denominator2 ; 
+	   } else if (operand == '/'){ // handles the division of fractions
+		   numerator = numerator1 * denominator2 ; 
+		   denominator = numerator2 * denominator1; 
+		   if (numerator < 0 && denominator < 0 ) { // divides with numerator and denom are negative
+			   numerator = Math.abs(numerator);
+		   } else if (denominator < 0 ) { // handles if only the denominator is negative
+			   numerator = -1 * numerator; 
+			   denominator = Math.abs(denominator);
+		   }
+	   } else { // error handling
+		   System.out.println("Your operator value was innappropriate. Please try again.");
+		   return "";
+	   }
+	   boolean isNegative = false; // used to tell if the end result will be positive or negative . 
+	   if (numerator < 0 ) { // if the numerator is negative, reminds us before we use abs values
+		   isNegative = true ; 
+	   }
+	   numerator = Math.abs(numerator);
+	   denominator = Math.abs(denominator);
+	   int[] endValues = { 0 , 0 , 0 } ; // stores final calculated values. 0-whole 1-numer 2-denom 
+	   // stores the remainder of the numerator divided by the denominator 
+	   int remainder = numerator % denominator ; 
+	   endValues[0] = (numerator - remainder) / denominator ; 
+	   for (int i = remainder ; i > 0 ; i-- ) { // reduces the fraction if the values match
+		   if (remainder % i == 0 && denominator % i == 0 ) { // checks if values match to reduce
+			   remainder = remainder / i ; 
+			   denominator = denominator / i ; 
+		   }
+	   }
+	   endValues[1] = remainder ; 
+	   endValues[2] = denominator ; 
+	   // changes the values of the product if the number is supposed to be negative
+	   if (isNegative == true ) { 
+		   if (endValues[0] == 0) { // changes the numerator value if the whole number is 0
+			   int value = -1 * endValues[1] ; // stores negative value of the numerator
+			   endValues[1] = value;
+		   } else { // changes the whole number value if there is one
+			   int value = (-1) * endValues[0] ; // stores negative value of the whole number
+			   endValues[0] = value ; 
+			   int value2 = Math.abs(endValues[1]) ; 
+			   endValues[1] = value2 ; 
+		   }
+	   }
+	   
+	   String answer = "" ; // stores the final string for the answer
+	   if (endValues[0] == 0 && endValues[1] == 0 ) {
+		   answer = "0" ; 
+   	   }  else if (endValues[0] == 0 ) { // handles if the answer is only a fraction
+		   answer = endValues[1] + "/" + endValues[2] ; 
+	   } else if(endValues[1] == 0 ) {// handles if the answer is only a whole number
+		   answer = "" + endValues[0] ; 
+	   } else { // handles all mixed numbers
+		   answer = endValues[0] + "_" + endValues[1] + "/" + endValues[2] ; 
+	   }
+	   return answer ; 
+   }
 }
